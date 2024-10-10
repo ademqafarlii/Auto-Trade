e22c4f241ca494724337c3417cd2ccccee6939be
Adəm Qafarlı <ademqafarli77@gmail.com>
2024-10-11 00:56:14 +0400

    exception handling methods updated

--- src/main/java/org/adem/autotrade/exception/GlobalExceptionHandler.java
+++ src/main/java/org/adem/autotrade/exception/GlobalExceptionHandler.java
@@ -1,53 +1,168 @@
 package org.adem.autotrade.exception;
 
-import org.adem.autotrade.repository.AdvantageRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CarNotFoundException.class)
-    public Map<String, String> handleCarNotFoundException(CarNotFoundException carNotFoundException) {
-        Map<String, String> map = new HashMap<>();
-        map.put("Message:", carNotFoundException.getMessage());
-        return map;
+    public ResponseEntity<ErrorResponse> handleCarNotFoundException(CarNotFoundException e, WebRequest webRequest) {
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", e.getMessage());
+
+        log.error("Car not found exception", e);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
 
     @ExceptionHandler(AnnouncementNotFoundException.class)
-    public Map<String, String> handleAnnouncementNotFoundException(AnnouncementNotFoundException announcementNotFoundException) {
-        Map<String, String> map = new HashMap<>();
-        map.put("Message:", announcementNotFoundException.getMessage());
-        return map;
+    public ResponseEntity<ErrorResponse> handleAnnouncementNotFoundException(AnnouncementNotFoundException e,
+                                                                             WebRequest webRequest) {
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", e.getMessage());
+
+        log.error("Announcement not found exception", e);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
 
     @ExceptionHandler(EmailAlreadyInUseException.class)
-    public Map<String, String> handleEmailAlreadyInUseException(EmailAlreadyInUseException emailAlreadyInUseException) {
-        Map<String, String> map = new HashMap<>();
-        map.put("Message:", emailAlreadyInUseException.getMessage());
-        return map;
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyInUseException(EmailAlreadyInUseException e,
+                                                                          WebRequest webRequest) {
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", e.getMessage());
+
+        log.error("Email already in use exception", e);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
-    public Map<String, String> handleInvalidCredentials(InvalidCredentialsException invalidCredentialsException) {
-        Map<String, String> map = new HashMap<>();
-        map.put("Message:", invalidCredentialsException.getMessage());
-        return map;
+    public ResponseEntity<ErrorResponse> handleInvalidCredentials(InvalidCredentialsException e, WebRequest webRequest) {
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", e.getMessage());
+
+        log.error("Invalid credentials exception", e);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
+
     @ExceptionHandler(UserNotFoundException.class)
-    public Map<String, String> handleUserNotFoundException(UserNotFoundException userNotFoundException) {
-        Map<String, String> map = new HashMap<>();
-        map.put("Message:", userNotFoundException.getMessage());
-        return map;
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException e, WebRequest webRequest) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", e.getMessage());
+
+        log.error("User not found exception", e);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
 
     @ExceptionHandler(AdvantageNotFoundException.class)
-    public Map<String, String> handleAdvantageNotFoundException(AdvantageNotFoundException advantageNotFoundException) {
-        Map<String, String> map = new HashMap<>();
-        map.put("Message:", advantageNotFoundException.getMessage());
-        return map;
+    public ResponseEntity<ErrorResponse> handleAdvantageNotFoundException(AdvantageNotFoundException e,
+                                                                          WebRequest webRequest) {
+
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", e.getMessage());
+
+        log.error("Advantage not found exception", e);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
     }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e,
+                                                                      WebRequest webRequest) {
+
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", e.getMessage());
+
+        log.error("No such element exception", e);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e,
+                                                                               WebRequest webRequest) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(fieldError ->
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        log.error("Field is not valid : {}", errors);
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(errors)
+                .path(webRequest.getHeader("Content-Type"))
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(LocalDate.now())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
+
 }
